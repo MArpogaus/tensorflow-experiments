e065ca9e1e0af0f319c2827106ab30f7577730ad
Marcel Arpogaus <marcel@arpogaus.de>
2021-02-10 20:27:04 +0100

    adds apbspathjoin tag to extended yaml loader

--- src/tfexp/utils/extended_loader.py
+++ src/tfexp/utils/extended_loader.py
@@ -1,3 +1,34 @@
+#!env python3
+# AUTHOR INFORMATION ##########################################################
+# file    : extended_loader.py
+# brief   : [Description]
+#
+# author  : Marcel Arpogaus
+# created : 2020-05-18 09:29:39
+# changed : 2020-11-15 19:41:35
+# DESCRIPTION #################################################################
+#
+# This project is following the PEP8 style guide:
+#
+#    https://www.python.org/dev/peps/pep-0008/)
+#
+# COPYRIGHT ###################################################################
+# Copyright 2020 Marcel Arpogaus
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# REQUIRED PYTHON MODULES #####################################################
 import os
 import re
 import yaml
@@ -24,9 +55,11 @@ class ExtendedLoader(yaml.Loader):
         self.set_global_variable_pattern = re.compile(r'^(\w+)\s*<=\s*(.+)$')
 
         self.add_constructor('!datetime', self._date_time_constructor)
+        self.add_constructor('!abspath', self._abspath_constructor)
         self.add_constructor('!product', self._np_product_constructor)
         self.add_constructor('!sum', self._np_sum_constructor)
         self.add_constructor('!join', self._join_constructor)
+        self.add_constructor('!abspathjoin', self._abspath_join_constructor)
         self.add_constructor('!include', self._include_constructor)
         self.add_constructor('!store',
                              self._set_global_variable_constructor)
@@ -46,6 +79,10 @@ class ExtendedLoader(yaml.Loader):
         now = dt.datetime.now()
         return value.format(now=now)
 
+    def _abspath_constructor(self, loader, node):
+        path = os.path.join(loader._root, self.construct_scalar(node))
+        return os.path.abspath(path)
+
     def _np_product_constructor(self, loader, node):
         value = loader.construct_sequence(node)
         return np.product(value)
@@ -57,6 +94,11 @@ class ExtendedLoader(yaml.Loader):
     def _join_constructor(self, loader, node):
         seq = loader.construct_sequence(node)
         return ''.join([str(i) for i in seq])
+
+    def _abspath_join_constructor(self, loader, node):
+        seq = loader.construct_sequence(node)
+        path = os.path.join(loader._root, *[str(i) for i in seq])
+        return os.path.abspath(path)
 
     def _include_constructor(self, loader, node):
         path = os.path.join(loader._root, self.construct_scalar(node))
