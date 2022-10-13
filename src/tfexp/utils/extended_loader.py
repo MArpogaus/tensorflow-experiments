47510dcc53737bb70f33674c1851e2a666f65658
Marcel Arpogaus <38564291+MArpogaus@users.noreply.github.com>
2022-10-13 11:31:37 +0200

    adds ExtendedLoader as class defintion

--- src/tfexp/utils/extended_loader.py
+++ src/tfexp/utils/extended_loader.py
@@ -39,106 +39,108 @@ import numpy as np
 import yaml
 
 
+class ExtendedLoader(yaml.Loader):
+    """YAML Loader with additional constructors and global variables."""
+
+    GLOBAL_VARIABLES = {}
+
+    def __init__(self, stream: IO) -> None:
+        super().__init__(stream)
+
+        try:
+            self._root = os.path.split(stream.name)[0]
+        except AttributeError:
+            self._root = os.path.curdir
+
+        self.get_global_variable_pattern = re.compile(r"^\$(\w+)$")
+        self.set_global_variable_pattern = re.compile(r"^(\w+)\s*<=\s*(.+)$")
+
+        self.add_constructor("!datetime", self._date_time_constructor)
+        self.add_constructor("!abspath", self._abspath_constructor)
+        self.add_constructor("!product", self._np_product_constructor)
+        self.add_constructor("!sum", self._np_sum_constructor)
+        self.add_constructor("!join", self._join_constructor)
+        self.add_constructor("!pathjoin", self._path_join_constructor)
+        self.add_constructor("!abspathjoin", self._abspath_join_constructor)
+        self.add_constructor("!include", self._include_constructor)
+
+        self.add_constructor("!store", self._set_global_variable_constructor)
+        self.add_constructor("!load", self._get_global_variable_constructor)
+
+        self.add_implicit_resolver("!store", self.set_global_variable_pattern, None)
+
+        self.add_implicit_resolver("!load", self.get_global_variable_pattern, None)
+
+    def _date_time_constructor(self, loader, node):
+        value = loader.construct_scalar(node)
+        now = dt.datetime.now()
+        return value.format(now=now)
+
+    def _abspath_constructor(self, loader, node):
+        path = os.path.join(loader._root, self.construct_scalar(node))
+        return os.path.abspath(path)
+
+    def _np_product_constructor(self, loader, node):
+        value = loader.construct_sequence(node)
+        return np.product(value)
+
+    def _np_sum_constructor(self, loader, node):
+        value = loader.construct_sequence(node)
+        return np.sum(value)
+
+    def _join_constructor(self, loader, node):
+        seq = loader.construct_sequence(node)
+        return "".join([str(i) for i in seq])
+
+    def _path_join_constructor(self, loader, node):
+        seq = loader.construct_sequence(node)
+        path = os.path.join(*seq)
+        return path
+
+    def _abspath_join_constructor(self, loader, node):
+        path = self._path_join_constructor(loader, node)
+        path = os.path.join(loader._root, path)
+        return os.path.abspath(path)
+
+    def _include_constructor(self, loader, node):
+        path = os.path.abspath(os.path.join(loader._root, self.construct_scalar(node)))
+        with open(path, "r") as yf:
+            ref = yaml.load(yf, Loader=ExtendedLoader)
+        return ref
+
+    def _set_global_variable_constructor(self, loader, node):
+        if isinstance(node, yaml.ScalarNode):
+            scalar = loader.construct_scalar(node)
+            match = self.set_global_variable_pattern.match(scalar)
+            key, value = match.groups()
+        else:
+            seq = loader.construct_sequence(node)
+            key = seq.pop(0)
+            value = seq.pop(0)
+        self.GLOBAL_VARIABLES[key] = value
+
+        return value
+
+    def _get_global_variable_constructor(self, loader, node):
+        scalar = loader.construct_scalar(node)
+        match = self.get_global_variable_pattern.match(scalar)
+        key = match.groups()[0]
+        value = self.GLOBAL_VARIABLES[key]
+
+        return value
+
+
 def get_loader(cmd):
-    class ExtendedLoader(yaml.Loader):
-        """YAML Loader with additional constructors and global variables."""
-
-        GLOBAL_VARIABLES = {}
-
+    class CMDLoader(ExtendedLoader):
         def __init__(self, stream: IO) -> None:
             self.cmd = cmd
 
             super().__init__(stream)
 
-            try:
-                self._root = os.path.split(stream.name)[0]
-            except AttributeError:
-                self._root = os.path.curdir
-
-            self.get_global_variable_pattern = re.compile(r"^\$(\w+)$")
-            self.set_global_variable_pattern = re.compile(r"^(\w+)\s*<=\s*(.+)$")
-
-            self.add_constructor("!datetime", self._date_time_constructor)
-            self.add_constructor("!abspath", self._abspath_constructor)
-            self.add_constructor("!product", self._np_product_constructor)
-            self.add_constructor("!sum", self._np_sum_constructor)
-            self.add_constructor("!join", self._join_constructor)
-            self.add_constructor("!pathjoin", self._path_join_constructor)
-            self.add_constructor("!abspathjoin", self._abspath_join_constructor)
-            self.add_constructor("!include", self._include_constructor)
             self.add_constructor("!switchcmd", self._switchcmd_constructor)
-
-            self.add_constructor("!store", self._set_global_variable_constructor)
-            self.add_constructor("!load", self._get_global_variable_constructor)
-
-            self.add_implicit_resolver(
-                u"!store", self.set_global_variable_pattern, None
-            )
-
-            self.add_implicit_resolver(u"!load", self.get_global_variable_pattern, None)
-
-        def _date_time_constructor(self, loader, node):
-            value = loader.construct_scalar(node)
-            now = dt.datetime.now()
-            return value.format(now=now)
-
-        def _abspath_constructor(self, loader, node):
-            path = os.path.join(loader._root, self.construct_scalar(node))
-            return os.path.abspath(path)
-
-        def _np_product_constructor(self, loader, node):
-            value = loader.construct_sequence(node)
-            return np.product(value)
-
-        def _np_sum_constructor(self, loader, node):
-            value = loader.construct_sequence(node)
-            return np.sum(value)
-
-        def _join_constructor(self, loader, node):
-            seq = loader.construct_sequence(node)
-            return "".join([str(i) for i in seq])
-
-        def _path_join_constructor(self, loader, node):
-            seq = loader.construct_sequence(node)
-            path = os.path.join(*seq)
-            return path
-
-        def _abspath_join_constructor(self, loader, node):
-            path = self._path_join_constructor(loader, node)
-            path = os.path.join(loader._root, path)
-            return os.path.abspath(path)
-
-        def _include_constructor(self, loader, node):
-            path = os.path.abspath(
-                os.path.join(loader._root, self.construct_scalar(node))
-            )
-            with open(path, "r") as yf:
-                ref = yaml.load(yf, Loader=ExtendedLoader)
-            return ref
 
         def _switchcmd_constructor(self, loader, node):
             cases = self.construct_mapping(node, deep=True)
             return cases.get(self.cmd, cases.get("default", None))
 
-        def _set_global_variable_constructor(self, loader, node):
-            if isinstance(node, yaml.ScalarNode):
-                scalar = loader.construct_scalar(node)
-                match = self.set_global_variable_pattern.match(scalar)
-                key, value = match.groups()
-            else:
-                seq = loader.construct_sequence(node)
-                key = seq.pop(0)
-                value = seq.pop(0)
-            self.GLOBAL_VARIABLES[key] = value
-
-            return value
-
-        def _get_global_variable_constructor(self, loader, node):
-            scalar = loader.construct_scalar(node)
-            match = self.get_global_variable_pattern.match(scalar)
-            key = match.groups()[0]
-            value = self.GLOBAL_VARIABLES[key]
-
-            return value
-
-    return ExtendedLoader
+    return CMDLoader
